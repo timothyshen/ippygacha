8e15d62c4916451644574925613a7bc1b5ee4aff
timtimtim.eth <timothyshen1@hotmail.com>
2025-11-06 16:38:36 +0000

    test: onchainRaffle

--- contract/contracts/mocks/MockERC721.sol
+++ contract/contracts/mocks/MockERC721.sol
@@ -2,28 +2,33 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockERC721 is ERC721, Ownable {
+contract MockERC721 is ERC721 {
     uint256 private _tokenIdCounter;
 
     constructor(
         string memory name,
         string memory symbol
-    ) ERC721(name, symbol) Ownable(msg.sender) {}
-
-    function mint(address to, uint256 tokenId) external onlyOwner {
-        _mint(to, tokenId);
+    ) ERC721(name, symbol) {
+        _tokenIdCounter = 1;
     }
 
-    function mintAuto(address to) external onlyOwner returns (uint256) {
+    function mint(address to) external returns (uint256) {
         uint256 tokenId = _tokenIdCounter;
         _tokenIdCounter++;
         _mint(to, tokenId);
         return tokenId;
     }
 
+    function exists(uint256 tokenId) external view returns (bool) {
+        return _ownerOf(tokenId) != address(0);
+    }
+
     function burn(uint256 tokenId) external {
         _burn(tokenId);
+    }
+
+    function getCurrentTokenId() external view returns (uint256) {
+        return _tokenIdCounter;
     }
 }
